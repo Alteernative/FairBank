b2d2585dcddbaa4e0ff592a1dc70801e33d1d61f
Jdaini Mohammed Ali <jdaini.mohammed_ali@courrier.uqam.ca>
2024-06-23 17:32:52 -0400

    Showing only the user that's loggied in

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -17,11 +17,10 @@ export default function Dashboard() {
         console.log("Log out successfull")
       }
     )
-
   }
 
     const [myData, setMyData] = useState([]);
-
+    console.log(myData)
     const GetData = () => {
         AxiosInstance.get('users/').then((res) => {
             setMyData(res.data);
@@ -35,15 +34,17 @@ export default function Dashboard() {
     }, []);
 
 
-    return (
+  return (
     <>
       <h1>Dashboard</h1>
       <div>
-        {myData.length > 0 ? (
-          <div key={0}>
-            <p>ID: {myData[0].id}</p>
-            <p>Email: {myData[0].email}</p>
-            <p>Balance: {myData[0].balance}</p>
+        {myData ? (
+          <div>
+            <div>ID: {myData.id}</div>
+            <div>Email: {myData.email}</div>
+            <div>First Name: {myData.first_name}</div>
+            <div>Last Name: {myData.last_name}</div>
+            <div>Balance: {myData.balance}</div>
           </div>
         ) : (
           <p>Loading data...</p>
