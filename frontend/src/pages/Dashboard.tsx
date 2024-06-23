5a5151c7e626739b99eeaf7de19794f41be4d162
Jdaini Mohammed Ali <jdaini.mohammed_ali@courrier.uqam.ca>
2024-06-22 23:02:24 -0400

    Added a template in the Dashboard to get data from the backend

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -1,7 +1,37 @@
+import AxiosInstance from "../components/AxiosInstance.tsx";
+import {useEffect, useState} from "react";
+
 export default function Dashboard() {
-  return (
+
+    const [myData, setMyData] = useState([]);
+
+    const GetData = () => {
+        AxiosInstance.get('users/').then((res) => {
+            setMyData(res.data);
+        }).catch((error) => {
+            console.error("There was an error fetching the data!", error);
+        });
+    }
+
+    useEffect(() => {
+        GetData();
+    }, []);
+
+
+    return (
     <>
       <h1>Dashboard</h1>
+      <div>
+        {myData.length > 0 ? (
+          <div key={0}>
+            <p>ID: {myData[0].id}</p>
+            <p>Email: {myData[0].email}</p>
+            <p>Balance: {myData[0].balance}</p>
+          </div>
+        ) : (
+          <p>Loading data...</p>
+        )}
+      </div>
     </>
   );
 }
