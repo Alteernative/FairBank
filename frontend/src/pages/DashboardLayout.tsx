5091c5992a2c8e4329c12fced56d6ad7472dab54
Issam Khalladi <fc891922@ens.uqam.ca>
2024-06-24 18:46:34 -0400

    Add UserContextProvider to allow access to the user data

--- frontend/src/pages/DashboardLayout.tsx
+++ frontend/src/pages/DashboardLayout.tsx
@@ -1,12 +1,16 @@
 import Sidebar from "@/components/Sidebar";
 import UserPanel from "@/components/UserPanel";
+import { UserContextProvider } from "@/components/UserContext.tsx";
 
+// export default function DashboardLayout({ children }: { children: React.ReactNode }) {
 export default function Dashboard({ children }: { children: React.ReactNode }) {
   return (
     <section className="flex w-full mb-5 h-screen">
-      <Sidebar />
-      {children}
-      <UserPanel />
+      <UserContextProvider>
+        <Sidebar />
+        {children}
+        <UserPanel />
+      </UserContextProvider>
     </section>
   );
 }
