325c7d9f305ed569eda941bd264d828d52aa7fd6
Issam Khalladi <fc891922@ens.uqam.ca>
2024-06-24 19:38:43 -0400

    Add transactions section in dashboard

---
+++ frontend/src/pages/DashboardTransactions.tsx
@@ -0,0 +1,10 @@
+export default function DashboardTransactions() {
+  return (
+    <main className="w-7/12 px-10 shadow-lg rounded-lg h-full">
+      <h1 className="text-6xl font-jomhuria mb-10">Transactions</h1>
+      <div className="flex items-center justify-center mt-52">
+        <p className="text-3xl font-semibold">Coming Soon</p>
+      </div>
+    </main>
+  );
+}
