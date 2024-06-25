5cbe200d1c01083139f9cee70b258b96b982a0ce
Issam Khalladi <fc891922@ens.uqam.ca>
2024-06-25 12:15:49 -0400

    Add autofocus to the first input field

--- frontend/src/pages/SignIn.tsx
+++ frontend/src/pages/SignIn.tsx
@@ -74,6 +74,7 @@ export default function Home() {
                   id="email"
                   placeholder="nom@exemple.com"
                   {...register("email")}
+                  autoFocus
                   required
                 />
                 <Input
