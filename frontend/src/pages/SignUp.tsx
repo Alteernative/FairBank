4736e3a767f1bfa5259b0e882504127dc6079b94
Issam Khalladi <fc891922@ens.uqam.ca>
2024-06-25 12:56:21 -0400

    Undo commit

--- frontend/src/pages/SignUp.tsx
+++ frontend/src/pages/SignUp.tsx
@@ -8,29 +8,14 @@ import {
 } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import { useForm } from "react-hook-form";
 import { Link } from "react-router-dom";
+import { useForm } from "react-hook-form";
 import { useNavigate } from "react-router-dom";
 import AxiosInstance from "../components/AxiosInstance.tsx";
-import { useMultiStepForm } from "@/hooks/useMultiStepFrom.ts";
-import UserForm from "./signup/UserForm.tsx";
-import PasswordForm from "./signup/PasswordForm.tsx";
-import PlanForm from "./signup/PlanForm.tsx";
-import EmailForm from "./signup/EmailForm.tsx";
-
-// DEBUG: This variable is unused?
-// import { Simulate } from "react-dom/test-utils";
-// import error = Simulate.error;
+import { Simulate } from "react-dom/test-utils";
+import error = Simulate.error;
 
 export default function Home() {
-  /*const { steps, currentStepIndex, step, isFirstStep, isLastStep, back, next } =
-    useMultistepForm([
-      <EmailForm />,
-      <UserForm />,
-      <PasswordForm />,
-      <PlanForm />,
-    ]);
-
   const { handleSubmit, register } = useForm();
   const navigate = useNavigate();
   const submission = (data) => {
@@ -58,40 +43,7 @@ export default function Home() {
           console.error("Error response:", error.response);
         }
       });
-  };*/
-
-  const navigate = useNavigate();
-  const steps = [
-    <EmailForm onSubmit={nextStep} />,
-    <PasswordForm onSubmit={nextStep} />,
-    <UserForm onSubmit={nextStep} />,
-    <PlanForm onSubmit={handleFinalSubmit} />,
-  ];
-  const { currentStepIndex, step, next, back } = useMultiStepForm(steps);
-
-  function nextStep(data) {
-    // Handle the form data of the current step
-    console.log(data);
-    next();
-  }
-
-  function handleFinalSubmit(data) {
-    console.log(data);
-    AxiosInstance.post("register/", data, {
-      headers: {
-        "Content-Type": "application/json",
-      },
-    })
-      .then(() => {
-        console.log("registered successfully");
-        navigate(`/connexion`);
-      })
-      .catch((error) => {
-        if (error.response) {
-          console.error("Error response:", error.response);
-        }
-      });
-  }
+  };
 
   return (
     <section className="flex h-screen">
@@ -113,8 +65,67 @@ export default function Home() {
           </Link>
         </div>
         {/* <Card className="w-96 h-[25rem] mt-52"> */}
-        {step}
-        {currentStepIndex > 0 && <Button onClick={back}>Back</Button>}
+        <Card className="w-96 h-[25rem] mt-52 border-none shadow-none">
+          <CardHeader>
+            <CardTitle className="text-2xl text-center">
+              Ouvrir un Compte Courant
+            </CardTitle>
+            <CardDescription className="text-center">
+              Entrer votre email ci-dessous pour créer votre compte
+            </CardDescription>
+          </CardHeader>
+          <CardContent>
+            {/* <form onSubmit={handleFormSubmit()}> */}
+            <form onSubmit={handleSubmit(submission)}>
+              <div className="flex flex-col gap-2">
+                <div className="flex flex-col gap-3">
+                  <Label htmlFor="prenom">Prenom</Label>
+                  <Input
+                    type="text"
+                    id="first_name"
+                    placeholder="Prenom"
+                    required
+                    {...register("first_name")}
+                  />
+                  <Label htmlFor="name">Nom</Label>
+                  <Input
+                    type="text"
+                    id="last_name"
+                    placeholder="Nom"
+                    {...register("last_name")}
+                    required
+                  />
+                  <Label htmlFor="email">Courriel</Label>
+                  <Input
+                    type="email"
+                    id="email"
+                    placeholder="nom@exemple.com"
+                    required
+                    {...register("email")}
+                  />
+                  <Label htmlFor="password">Mot de passe</Label>
+                  <Input
+                    type="password"
+                    id="password"
+                    placeholder="•••••••••"
+                    {...register("password")}
+                    required
+                  />
+                  <Label htmlFor="password">Confirmer le mot de passe</Label>
+                  <Input
+                    type="password"
+                    id="repassword2"
+                    placeholder="•••••••••"
+                    required
+                  />
+                  <Button type="submit" className="mt-2">
+                    Se connecter avec l'email
+                  </Button>
+                </div>
+              </div>
+            </form>
+          </CardContent>
+        </Card>
       </section>
     </section>
   );
