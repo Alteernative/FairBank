733d871b718b2ad1fa0c0402769611abb45235b9
Jdaini Mohammed Ali <jdaini.mohammed_ali@courrier.uqam.ca>
2024-07-01 23:16:34 -0400

    Set up the new DB to request a transaction

--- backend/users/models.py
+++ backend/users/models.py
@@ -107,11 +107,11 @@ class Transaction(models.Model):
 
 
 class PendingTrasactions(models.Model):
-    sender = models.ForeignKey(CustomUser, related_name='pending_sent_transactions', on_delete=models.CASCADE)
+    sender = models.ForeignKey(CustomUser, related_name='pending_sender_transactions', on_delete=models.CASCADE)
     receiver = models.ForeignKey(CustomUser, related_name='pending_received_transactions', on_delete=models.CASCADE)
     STATUS_CHOICES = [
-        ('pending', 'Pending'),
-        ('accepted', 'Accepted'),
+        ('pending', 'pending'),
+        ('accepted', 'accepted'),
     ]
     status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='pending')
     amount = models.DecimalField(max_digits=10, decimal_places=2)
