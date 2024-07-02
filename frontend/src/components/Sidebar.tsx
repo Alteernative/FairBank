6ca142d6ae0e66843bc98c1d95148f335d10a434
Issam Khalladi <fc891922@ens.uqam.ca>
2024-07-02 19:00:13 -0400

    Fix sidebar button sizes to have same size

--- frontend/src/components/Sidebar.tsx
+++ frontend/src/components/Sidebar.tsx
@@ -29,80 +29,88 @@ export default function Sidebar() {
   };
 
   return (
-    <section className="flex h-full w-2/12 flex-col items-center">
+    <aside className="flex h-full w-2/12 flex-col items-center">
       <Link to={"/"}>
         <h1 className="font-jomhuria text-6xl">FairBank</h1>
       </Link>
 
-      <div className="mt-10 flex flex-col gap-5">
-        <Link to={"/dashboard"}>
-          <Button
-            variant={isActive("/dashboard") ? "default" : "ghost"}
-            className="flex w-full items-center justify-start gap-2"
-          >
-            <PiCirclesFourFill />
-            Overview
-          </Button>
-        </Link>
-        <Link to={"/dashboard/transactions"}>
-          <Button
-            variant={isActive("/dashboard/transactions") ? "default" : "ghost"}
-            className="flex w-full items-center justify-start gap-2"
-          >
-            <IoDocumentTextOutline />
-            Transactions
-          </Button>
-        </Link>
-        <Link to={"/dashboard/compare-plans"}>
-          <Button
-            variant={isActive("/dashboard/compare-plans") ? "default" : "ghost"}
-            className="flex w-full items-center justify-start gap-2"
-          >
-            <MdCompareArrows />
-            Comparer les plans
-          </Button>
-        </Link>
-        <Link to={"/dashboard/exchange-rates"}>
-          <Button
-            variant={
-              isActive("/dashboard/exchange-rates") ? "default" : "ghost"
-            }
-            className="flex w-full items-center justify-start gap-2"
-          >
-            <BsCurrencyExchange />
-            Taux de change
-          </Button>
-        </Link>
-      </div>
+      <nav className="mb-5 mt-10 flex h-full flex-col justify-between">
+        {/* Top nav */}
+        <div className="flex flex-col gap-5">
+          <Link to={"/dashboard"}>
+            <Button
+              variant={isActive("/dashboard") ? "default" : "ghost"}
+              className="flex w-full items-center justify-start gap-2"
+            >
+              <PiCirclesFourFill />
+              Overview
+            </Button>
+          </Link>
+          <Link to={"/dashboard/transactions"}>
+            <Button
+              variant={
+                isActive("/dashboard/transactions") ? "default" : "ghost"
+              }
+              className="flex w-full items-center justify-start gap-2"
+            >
+              <IoDocumentTextOutline />
+              Transactions
+            </Button>
+          </Link>
+          <Link to={"/dashboard/compare-plans"}>
+            <Button
+              variant={
+                isActive("/dashboard/compare-plans") ? "default" : "ghost"
+              }
+              className="flex w-full items-center justify-start gap-2"
+            >
+              <MdCompareArrows />
+              Comparer les plans
+            </Button>
+          </Link>
+          <Link to={"/dashboard/exchange-rates"}>
+            <Button
+              variant={
+                isActive("/dashboard/exchange-rates") ? "default" : "ghost"
+              }
+              className="flex w-full items-center justify-start gap-2"
+            >
+              <BsCurrencyExchange />
+              Taux de change
+            </Button>
+          </Link>
+        </div>
 
-      <div className="mb-5 mt-auto flex flex-col gap-5">
-        <Link to={"/dashboard/help"}>
+        {/* Bottom nav*/}
+        <div className="flex flex-col gap-5">
+          <Link to={"/dashboard/help"}>
+            <Button
+              variant={isActive("/dashboard/help") ? "default" : "ghost"}
+              className="flex w-full items-center justify-start gap-2"
+            >
+              <FaRegQuestionCircle />
+              Aide
+            </Button>
+          </Link>
+          <Link to={"/dashboard/settings"}>
+            <Button
+              variant={isActive("/dashboard/settings") ? "default" : "ghost"}
+              className="flex w-full items-center justify-start gap-2"
+            >
+              <FaCog />
+              Paramètres
+            </Button>
+          </Link>
           <Button
-            variant={isActive("/dashboard/help") ? "default" : "ghost"}
+            variant={"destructive"}
+            onClick={handleLogout}
             className="flex w-full items-center justify-start gap-2"
           >
-            <FaRegQuestionCircle />
-            Aide
+            <FaSignOutAlt />
+            Se déconnecter
           </Button>
-        </Link>
-        <Link to={"/dashboard/settings"}>
-          <Button
-            variant={isActive("/dashboard/settings") ? "default" : "ghost"}
-            className="flex w-full items-center justify-start gap-2"
-          >
-            <FaCog />
-            Paramètres
-          </Button>
-        </Link>
-        <Button
-          variant={"destructive"}
-          onClick={handleLogout}
-          className="flex w-full items-center justify-start gap-2"
-        >
-          <FaSignOutAlt />
-          Se déconnecter
-        </Button>
-      </div>
-    </section>
+        </div>
+      </nav>
+    </aside>
   );
 }
