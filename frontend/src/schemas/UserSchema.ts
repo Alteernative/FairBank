636b472a14b437a4eda51fa5ab7dca565cbd06b6
Issam Khalladi <fc891922@ens.uqam.ca>
2024-07-03 00:36:52 -0400

    Add signUpSchema

--- frontend/src/schemas/UserSchema.ts
+++ frontend/src/schemas/UserSchema.ts
@@ -1,6 +1,6 @@
 import { z } from "zod";
 
-export const SignInSchema = z.object({
+export const signInSchema = z.object({
   email: z.string().email({
     message: "Adresse courriel invalide.",
   }),
@@ -9,7 +9,44 @@ export const SignInSchema = z.object({
   password: z.string({
     message: "Mot de passe invalide.",
   }),
-  // password: z.string().min(8, {
-  //   message: "Mot de passe doit contenir au moins 8 caractères.",
-  // }),
 });
+
+export const signUpSchema = z
+  .object({
+    email: z.string().email({
+      message: "Adresse courriel invalide.",
+    }),
+
+    first_name: z.string().min(1, {
+      message: "Prénom invalide.",
+    }),
+
+    last_name: z.string().min(1, {
+      message: "Nom invalide.",
+    }),
+
+    age: z.number().gte(18, {
+      message: "Âge invalide. (18+)",
+    }),
+
+    password: z
+      .string()
+      .min(8, {
+        message: "Mot de passe doit contenir au moins 8 caractères.",
+      })
+      .regex(/[A-Z]/, {
+        message: "Mot de passe doit contenir au moins une majuscule",
+      })
+      .regex(/[a-z]/, {
+        message: "Mot de passe doit contenir au moins une minuscule",
+      })
+      .regex(/[`!@#$%^&*()_\-+=\[\]{};':"\\|,.<>\/?~ ]/, {
+        message: "Mot de passe doit contenir au moins un caractère spécial.",
+      }),
+
+    rePassword: z.string(),
+  })
+  .refine((data) => data.password === data.rePassword, {
+    message: "Les mots de passe ne sont pas identiques.",
+    path: ["rePassword"],
+  });
