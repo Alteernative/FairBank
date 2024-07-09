179430e7a69ba4c491b20e9d49f8fcf1452f55f4
Issam Khalladi <fc891922@ens.uqam.ca>
2024-07-09 16:21:08 -0400

    Remove debug code

--- frontend/src/pages/signup/StepWrapper.tsx
+++ frontend/src/pages/signup/StepWrapper.tsx
@@ -22,9 +22,7 @@ export default function StepWrapper({
   isLastStep,
 }: StepWrapperProps) {
   return (
-    // DEBUG: Remove border debugger
     <Card className="h-[25rem] w-96 border-none shadow-none">
-      {/* <Card className="h-[25rem] w-96 border shadow-md"> */}
       <CardHeader>
         <CardTitle className="text-center text-2xl">{title}</CardTitle>
         <CardDescription className="text-center">{description}</CardDescription>
@@ -32,7 +30,7 @@ export default function StepWrapper({
       <CardContent className="flex flex-col justify-between">
         {children}
         <div className="mt-6 flex w-full">
-          {/* <Button type="submit" onClick={onNext} className="ml-auto"> */}
+          {/* <Button type="submit" disabled={isSubmitting} className="ml-auto"> */}
           <Button type="submit" className="ml-auto">
             {isLastStep ? "Soumettre" : "Suivant"}
           </Button>
