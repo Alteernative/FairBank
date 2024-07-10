0f50906bee190841916258c5f32a65fe95fb059d
Issam Khalladi <fc891922@ens.uqam.ca>
2024-07-10 00:27:22 -0400

    Update error interceptor

--- frontend/src/components/AxiosInstance.tsx
+++ frontend/src/components/AxiosInstance.tsx
@@ -1,46 +1,41 @@
-import axios from 'axios'
+import axios from "axios";
 
-const baseURl = 'http://127.0.0.1:8000/'
+const baseURl = "http://127.0.0.1:8000/";
 
 const AxiosInstance = axios.create({
-
-    baseURL: baseURl,
-    timeout: 5000,
-    headers: {
-        "Content-Type": "application/json",
-        accept: "application/json",
-    }
-
-})
+  baseURL: baseURl,
+  timeout: 5000,
+  headers: {
+    "Content-Type": "application/json",
+    accept: "application/json",
+  },
+});
 
 AxiosInstance.interceptors.request.use(
-    (config) => {
-
-        const token = localStorage.getItem('Token')
-
-        if (token) {
-            config.headers.Authorization = `Token ${token}`
-        } else {
-            config.headers.Authorization = ``
-        }
-        return config
-    },
-
-)
+  (config) => {
+    const token = localStorage.getItem("Token");
+    if (token) {
+      config.headers.Authorization = `Token ${token}`;
+    } else {
+      config.headers.Authorization = ``;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
 
 AxiosInstance.interceptors.response.use(
-
-    (response) =>{
-
-        return response
-
-    },
-    (error)=>{
-        if(error.response && error.response.status === 401){
-            localStorage.removeItem('Token')
-        }
+  (response) => {
+    return response;
+  },
+  (error) => {
+    if (error.response && error.response.status === 401) {
+      localStorage.removeItem("Token");
     }
+    return Promise.reject(error);
+  }
+);
 
-)
-
-export default AxiosInstance
+export default AxiosInstance;
