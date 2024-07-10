a63883a5b538e9e9a8f090eeb790576a316408d4
Issam Khalladi <fc891922@ens.uqam.ca>
2024-07-10 19:58:28 -0400

    Update name validation and Add date of birth validation

--- frontend/src/schemas/SignUpSchema.ts
+++ frontend/src/schemas/SignUpSchema.ts
@@ -1,54 +1,17 @@
-// import { z } from "zod";
-
-// export const emailSchema = z.object({
-//   email: z.string().email({
-//     message: "Adresse courriel invalide.",
-//   }),
-// });
-
-// export const passwordSchema = z
-//   .object({
-//     password: z
-//       .string()
-//       .min(8, {
-//         message: "Mot de passe doit contenir au moins 8 caractères.",
-//       })
-//       .regex(/[A-Z]/, {
-//         message: "Mot de passe doit contenir au moins une majuscule",
-//       })
-//       .regex(/[a-z]/, {
-//         message: "Mot de passe doit contenir au moins une minuscule",
-//       })
-//       .regex(/[`!@#$%^&*()_\-+=\[\]{};':"\\|,.<>\/?~ ]/, {
-//         message: "Mot de passe doit contenir au moins un caractère spécial.",
-//       }),
-//     rePassword: z.string(),
-//   })
-//   .refine((data) => data.password === data.rePassword, {
-//     message: "Les mots de passe ne sont pas identiques.",
-//     path: ["rePassword"],
-//   });
-
-// export const userSchema = z.object({
-//   first_name: z.string().min(1, {
-//     message: "Prénom invalide.",
-//   }),
-//   last_name: z.string().min(1, {
-//     message: "Nom invalide.",
-//   }),
-//   age: z.number().gte(18, {
-//     message: "Âge invalide. (18+)",
-//   }),
-// });
 import { z } from "zod";
 
 export const signUpSchema = (step) => {
   switch (step) {
     case 1:
       return z.object({
-        email: z.string().email({
-          message: "Adresse courriel invalide.",
-        }),
+        email: z
+          .string()
+          .min(1, {
+            message: "Adresse courriel requise.",
+          })
+          .email({
+            message: "Adresse courriel invalide.",
+          }),
       });
     case 2:
       return z
@@ -77,15 +40,34 @@ export const signUpSchema = (step) => {
     case 3:
       return z.object({
         first_name: z.string().min(1, {
-          message: "Prénom invalide.",
+          message: "Prénom requis.",
         }),
         last_name: z.string().min(1, {
-          message: "Nom invalide.",
+          message: "Nom requis.",
         }),
-        // TODO: Replace with birthday?
-        // age: z.number().gte(18, {
-        //   message: "Âge invalide. (18+)",
-        // }),
+      });
+    case 4:
+      return z.object({
+        birthday_year: z.coerce
+          .number({
+            invalid_type_error: "Année invalide.",
+          })
+          .min(1900, "Année invalide.")
+          .max(new Date().getFullYear()),
+
+        // TODO: Find a way to show an error when the month is not selected.
+        birthday_month: z.string({
+          required_error: "Sélectionner le mois.",
+        }),
+
+        birthday_day: z.coerce
+          .number({ invalid_type_error: "Jour invalide." })
+          .min(1, {
+            message: "Jour invalide.",
+          })
+          .max(31, {
+            message: "Jour invalide.",
+          }),
       });
   }
 };
