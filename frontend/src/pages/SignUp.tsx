7d4d309e18d67f5edab5113481032c9d0af34d7d
Issam Khalladi <fc891922@ens.uqam.ca>
2024-07-10 23:00:22 -0400

    Add toast and sign up refresh in case of error

--- frontend/src/pages/SignUp.tsx
+++ frontend/src/pages/SignUp.tsx
@@ -9,7 +9,7 @@ import EmailForm from "./signup/EmailForm.tsx";
 import PasswordForm from "./signup/PasswordForm.tsx";
 import NameForm from "./signup/NameForm.tsx";
 import BirthdayForm from "./signup/BirthdayForm.tsx";
-import { toast } from "sonner";
+import { toast, Toaster } from "sonner";
 // import PlanForm from "./signup/PlanForm.tsx";
 
 type FormValues = {
@@ -67,10 +67,17 @@ export default function SignUp() {
           console.error("Error status:", error.response.status);
           console.error("Errors data:", responseErrorData);
 
-          if (error.response.status === 400) {
-            console.log("Inside toast if");
-            toast("Inscription échoué. Courriel existe déjà.");
-            navigate("/inscription");
+          if (
+            error.response.status === 400 &&
+            responseErrorData.email.includes(
+              "user with this email already exists."
+            )
+          ) {
+            console.log("Email already taken");
+            toast.error("Inscription refusé, le courriel existe déjà.");
+            setTimeout(() => {
+              window.location.reload();
+            }, 3500);
           }
         }
       }
@@ -78,48 +85,51 @@ export default function SignUp() {
   };
 
   return (
-    <section className="flex h-screen">
-      <aside className="hidden w-full flex-1 flex-col bg-[#efeee6] lg:flex">
-        <Link to={"/"} className="ml-8 mt-7 flex items-center">
-          <h1 className="font-jomhuria text-6xl">FairBank</h1>
-        </Link>
-        <img
-          src="/login.svg"
-          alt="Sign in image"
-          className="my-auto w-full content-center overflow-hidden"
-        />
-      </aside>
-      <main className="flex w-80 flex-1 items-center justify-center bg-white">
-        <Button
-          asChild
-          variant={"ghost"}
-          className="absolute right-0 top-0 m-5"
-        >
-          <Link to={"/connexion"}>Se connecter</Link>
-        </Button>
-        <h1 className="absolute left-7 top-7 font-jomhuria text-6xl lg:hidden">
-          <Link to={"/"}>FairBank</Link>
-        </h1>
+    <>
+      <section className="flex h-screen">
+        <aside className="hidden w-full flex-1 flex-col bg-[#efeee6] lg:flex">
+          <Link to={"/"} className="ml-8 mt-7 flex items-center">
+            <h1 className="font-jomhuria text-6xl">FairBank</h1>
+          </Link>
+          <img
+            src="/login.svg"
+            alt="Sign in image"
+            className="my-auto w-full content-center overflow-hidden"
+          />
+        </aside>
+        <main className="flex w-80 flex-1 items-center justify-center bg-white">
+          <Button
+            asChild
+            variant={"ghost"}
+            className="absolute right-0 top-0 m-5"
+          >
+            <Link to={"/connexion"}>Se connecter</Link>
+          </Button>
+          <h1 className="absolute left-7 top-7 font-jomhuria text-6xl lg:hidden">
+            <Link to={"/"}>FairBank</Link>
+          </h1>
 
-        <FormProvider {...methods}>
-          <form onSubmit={methods.handleSubmit(onSubmit)}>
-            {step === 1 && <EmailForm />}
-            {step === 2 && <PasswordForm />}
-            {step === 3 && <NameForm />}
-            {step === 4 && (
-              <BirthdayForm
-                isLastStep={true}
-                isSubmitting={methods.formState.isSubmitting}
-              />
-            )}
-            {/* {step === 5 && (
+          <FormProvider {...methods}>
+            <form onSubmit={methods.handleSubmit(onSubmit)}>
+              {step === 1 && <EmailForm />}
+              {step === 2 && <PasswordForm />}
+              {step === 3 && <NameForm />}
+              {step === 4 && (
+                <BirthdayForm
+                  isLastStep={true}
+                  isSubmitting={methods.formState.isSubmitting}
+                />
+              )}
+              {/* {step === 5 && (
               <PlanForm
                 isLastStep={true}
               />
             )} */}
-          </form>
-        </FormProvider>
-      </main>
-    </section>
+            </form>
+          </FormProvider>
+        </main>
+      </section>
+      <Toaster richColors duration={3000} />
+    </>
   );
 }
