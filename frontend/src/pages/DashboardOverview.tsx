36bf4ee421b2646b729a9436e98059040adfb6cb
Issam Khalladi <fc891922@ens.uqam.ca>
2024-07-14 13:44:41 -0400

    Fix dark mode

--- frontend/src/pages/DashboardOverview.tsx
+++ frontend/src/pages/DashboardOverview.tsx
@@ -36,7 +36,7 @@ export default function DashboardOverview() {
     <main className="h-full w-7/12 rounded-lg px-10 shadow-lg">
       <h1 className="mb-10 font-jomhuria text-6xl">Overview</h1>
       <div className="grid grid-cols-3 grid-rows-5 gap-4">
-        <div className="col-span-2 row-span-1 rounded-lg bg-white p-4 shadow">
+        <div className="col-span-2 row-span-1 rounded-lg border p-4 shadow">
           <h2 className="mb-3 font-bold">Balance</h2>
           <CountUp
             start={0}
@@ -48,22 +48,22 @@ export default function DashboardOverview() {
           />
         </div>
 
-        <div className="col-span-1 row-span-1 rounded-lg bg-white p-4 shadow">
+        <div className="col-span-1 row-span-1 rounded-lg border p-4 shadow">
           <h2>Graphe ratio des dépots et ajouts:</h2>
         </div>
 
-        <div className="col-span-2 row-span-3 flex h-full w-full flex-col rounded-lg border p-4 shadow">
+        <div className="col-span-2 row-span-3 flex h-full w-full flex-col rounded-lg border border p-4 shadow">
           <DashboardGraph />
         </div>
 
-        <div className="col-span-1 row-span-3 rounded-lg bg-white p-4 shadow">
+        <div className="col-span-1 row-span-3 rounded-lg border p-4 shadow">
           <h2>
             Transactions totales :{" "}
             {user.sent_transactions.length + user.received_transactions.length}
           </h2>
         </div>
 
-        <div className="col-span-3 row-span-1 rounded-lg bg-white p-4 shadow">
+        <div className="col-span-3 row-span-1 rounded-lg border p-4 shadow">
           <h2 className="mb-0 font-bold">Balance des devises étrangères:</h2>
 
           <Carousel className="w-full">
