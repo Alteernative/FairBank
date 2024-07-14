1fd5287ff1dde20aaa7f9a47f4c342a2456804aa
Issam Khalladi <fc891922@ens.uqam.ca>
2024-07-14 12:43:33 -0400

    Add dark mode

--- frontend/src/pages/Services.tsx
+++ frontend/src/pages/Services.tsx
@@ -30,7 +30,7 @@ export default function Services() {
 
       <section className="container mx-auto">
         <div className="grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3">
-          <div className="rounded-lg bg-white p-6 shadow-lg">
+          <div className="rounded-lg border p-6 dark:border-slate-900">
             <FaMoneyCheckAlt className="mb-4 text-4xl text-green-600" />
             <h2 className="mb-2 text-2xl font-semibold">
               Gestion de comptes bancaires
@@ -41,13 +41,13 @@ export default function Services() {
             </p>
           </div>
 
-          <div className="rounded-lg bg-white p-6 shadow-lg">
+          <div className="rounded-lg border p-6 dark:border-slate-900">
             <FaExchangeAlt className="mb-4 text-4xl text-green-600" />
             <h2 className="mb-2 text-2xl font-semibold">Transferts de fonds</h2>
             <p>Effectuez des transferts de fonds rapides et sécurisés.</p>
           </div>
 
-          <div className="rounded-lg bg-white p-6 shadow-lg">
+          <div className="rounded-lg border p-6 dark:border-slate-900">
             <FaReceipt className="mb-4 text-4xl text-green-600" />
             <h2 className="mb-2 text-2xl font-semibold">
               Paiements de factures
@@ -58,7 +58,7 @@ export default function Services() {
             </p>
           </div>
 
-          <div className="rounded-lg bg-white p-6 shadow-lg">
+          <div className="rounded-lg border p-6 dark:border-slate-900">
             <FaDollarSign className="mb-4 text-4xl text-green-600" />
             <h2 className="mb-2 text-2xl font-semibold">
               Taux de change actuels
@@ -68,13 +68,13 @@ export default function Services() {
             </p>
           </div>
 
-          <div className="rounded-lg bg-white p-6 shadow-lg">
+          <div className="rounded-lg border p-6 dark:border-slate-900">
             <FaWallet className="mb-4 text-4xl text-green-600" />
             <h2 className="mb-2 text-2xl font-semibold">Retraits gratuits</h2>
             <p>Retraits gratuits à n’importe quel ATM au Canada.</p>
           </div>
 
-          <div className="rounded-lg bg-white p-6 shadow-lg">
+          <div className="rounded-lg border p-6 dark:border-slate-900">
             <FaMoneyBillWave className="mb-4 text-4xl text-green-600" />
             <h2 className="mb-2 text-2xl font-semibold">
               Virements sans frais
@@ -82,7 +82,7 @@ export default function Services() {
             <p>Virements d'argent sans payer des frais de transfert.</p>
           </div>
 
-          <div className="rounded-lg bg-white p-6 shadow-lg">
+          <div className="rounded-lg border p-6 dark:border-slate-900">
             <FaPiggyBank className="mb-4 text-4xl text-green-600" />
             <h2 className="mb-2 text-2xl font-semibold">Remises en argent</h2>
             <p>
@@ -90,7 +90,7 @@ export default function Services() {
             </p>
           </div>
 
-          <div className="rounded-lg bg-white p-6 shadow-lg">
+          <div className="rounded-lg border p-6 dark:border-slate-900">
             <FaChartLine className="mb-4 text-4xl text-green-600" />
             <h2 className="mb-2 text-2xl font-semibold">
               Tableau de bord personnalisé
@@ -100,7 +100,7 @@ export default function Services() {
               revenus.
             </p>
           </div>
-          <div className="rounded-lg bg-white p-6 shadow-lg">
+          <div className="rounded-lg border p-6 dark:border-slate-900">
             <FaLock className="mb-4 text-4xl text-green-600" />
             <h2 className="mb-2 text-2xl font-semibold">Securite garantie</h2>
             <p>
