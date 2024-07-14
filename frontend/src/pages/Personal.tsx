f32046d46c734243a926a3660c64571d686778c0
Issam Khalladi <fc891922@ens.uqam.ca>
2024-07-13 23:37:40 -0400

    Update asset images colors

--- frontend/src/pages/Personal.tsx
+++ frontend/src/pages/Personal.tsx
@@ -6,7 +6,11 @@ export default function Particuliers() {
       <section className="py-12">
         <div className="container mx-auto text-center">
           <div className="flex justify-center">
-            <img src="/choix.svg" alt="" className="mb-8 h-auto w-1/4" />
+            <img
+              src="/images/personal.svg"
+              alt=""
+              className="mb-8 h-auto w-1/3"
+            />
           </div>
           <h1 className="mb-14 font-jomhuria text-6xl text-primary">
             Les choix pour Particuliers
@@ -14,24 +18,24 @@ export default function Particuliers() {
         </div>
       </section>
 
-      <section className="container mx-auto px-6 py-12">
+      <section className="container mx-auto">
         <div className="grid grid-cols-1 gap-8 md:grid-cols-3">
           <div className="rounded-lg bg-white p-6 shadow-lg">
-            <FaMoneyCheck className="mb-4 text-4xl text-green-600" />
+            <FaMoneyCheck className="mb-4 text-4xl text-[#66C02E]" />
             <h2 className="mb-2 text-2xl font-semibold">Comptes Régulier</h2>
             <p>Limite de transfert quotidiennes: 10 000$ / jour</p>
             <p>Transactions quotidiennes: 20</p>
             <p>Transactions quotidiennes: 20.99% </p>
           </div>
           <div className="rounded-lg bg-white p-6 shadow-lg">
-            <FaPiggyBank className="mb-4 text-4xl text-green-600" />
+            <FaPiggyBank className="mb-4 text-4xl text-[#66C02E]" />
             <h2 className="mb-2 text-2xl font-semibold">Comptes Premium</h2>
             <p>Limite de transfert quotidiennes: 50 000$ / jour </p>
             <p>Transactions quotidiennes: 50</p>
             <p>Taux d'interets sur les prets: 15.99%</p>
           </div>
           <div className="rounded-lg bg-white p-6 shadow-lg">
-            <FaCrown className="mb-4 text-4xl text-green-600" />
+            <FaCrown className="mb-4 text-4xl text-[#66C02E]" />
             <h2 className="mb-2 text-2xl font-semibold">Comptes Ultime</h2>
             <p>Limite de transfert quotidiennes: Illimité</p>
             <p>Transactions quotidiennes: Illimité</p>
