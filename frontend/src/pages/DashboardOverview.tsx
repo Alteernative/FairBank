e373fdf90981849897d077a5ce2fc476a057fd00
Issam Khalladi <fc891922@ens.uqam.ca>
2024-07-14 13:08:19 -0400

    Fix grid dark mode

--- frontend/src/pages/DashboardOverview.tsx
+++ frontend/src/pages/DashboardOverview.tsx
@@ -10,7 +10,7 @@ export default function DashboardOverview() {
     <main className="h-full w-7/12 rounded-lg px-10 shadow-lg">
       <h1 className="mb-10 font-jomhuria text-6xl">Overview</h1>
       <div className="grid grid-cols-3 grid-rows-5 gap-4">
-        <div className="col-span-2 row-span-1 rounded-lg bg-white p-4 shadow">
+        <div className="col-span-2 row-span-1 rounded-lg border p-4 shadow">
           <h2 className="mb-3 font-bold">Balance</h2>
           <CountUp
             start={0}
@@ -22,19 +22,19 @@ export default function DashboardOverview() {
           />
         </div>
 
-        <div className="col-span-1 row-span-1 rounded-lg bg-white p-4 shadow">
+        <div className="col-span-1 row-span-1 rounded-lg border p-4 shadow">
           <h2>Graphe ratio des depots et ajouts</h2>
         </div>
 
-        <div className="col-span-2 row-span-3 flex h-full w-full flex-col rounded-lg bg-white p-4 shadow">
+        <div className="col-span-2 row-span-3 flex h-full w-full flex-col rounded-lg border p-4 shadow">
           <DashboardGraph />
         </div>
 
-        <div className="col-span-1 row-span-3 rounded-lg bg-white p-4 shadow">
+        <div className="col-span-1 row-span-3 rounded-lg border p-4 shadow">
           <h2>Transactions rapide</h2>
         </div>
 
-        <div className="col-span-3 row-span-1 rounded-lg bg-white p-4 shadow">
+        <div className="col-span-3 row-span-1 rounded-lg border p-4 shadow">
           <h2>
             Transactions :{" "}
             {user.sent_transactions.length + user.received_transactions.length}
