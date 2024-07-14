16ad6c1678117586d33a0c41229903af9e32231a
Issam Khalladi <fc891922@ens.uqam.ca>
2024-07-14 13:10:09 -0400

    Fix dark mode

--- frontend/src/components/UserPanel.tsx
+++ frontend/src/components/UserPanel.tsx
@@ -343,7 +343,7 @@ export default function UserPanel() {
 
         <div className="mb-5 w-full">
           <h2 className="font-semibold">Activités récentes</h2>
-          <div className="space-y-2 rounded-lg bg-white p-2 shadow">
+          <div className="space-y-2 rounded-lg border p-2 shadow">
             {activities.map((activity, index) => (
               <div key={index} className="flex items-center justify-between">
                 <div>
@@ -361,7 +361,7 @@ export default function UserPanel() {
         </div>
         <div className="mb-5 w-full">
           <h2 className="font-semibold">Activités a accepter</h2>
-          <div className="space-y-2 rounded-lg bg-white p-2 shadow">
+          <div className="space-y-2 rounded-lg border p-2 shadow">
             {user?.pending_sender_transactions?.map(
               (transaction: Transaction, index: number) => (
                 <div key={index} className="flex items-center justify-between">
@@ -406,7 +406,7 @@ export default function UserPanel() {
           <h2 className="font-semibold">
             Activités en attente d'acceptation de l'autre
           </h2>
-          <div className="space-y-2 rounded-lg bg-white p-2 shadow">
+          <div className="space-y-2 rounded-lg border p-2 shadow">
             {user?.pending_received_transactions?.map(
               (transaction: Transaction, index: number) => (
                 <div key={index} className="flex items-center justify-between">
