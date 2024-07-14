5d16c384ec1a409524d9db18fa5447e18620d112
Issam Khalladi <fc891922@ens.uqam.ca>
2024-07-14 12:25:42 -0400

    Add theme button

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -7,6 +7,7 @@ import {
 } from "./ui/navigation-menu";
 import { useEffect, useState } from "react";
 import AxiosInstance from "@/components/AxiosInstance.tsx";
+import { ModeToggle } from "./ModeToggle";
 
 export default function Header() {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
@@ -39,8 +40,6 @@ export default function Header() {
   return (
     <header className="mt-7 flex items-center justify-between">
       <Link to="/" className="flex items-center">
-        {/* <img className="w-10" src="/logo_no_bg.png" alt="Logo du site" />
-          <h1 className="text-2xl font-bold font-sans">FairBank</h1> */}
         <h1 className="font-jomhuria text-6xl text-primary">FairBank</h1>
       </Link>
       <NavigationMenu>
@@ -79,7 +78,8 @@ export default function Header() {
           </NavigationMenuItem>
         </NavigationMenuList>
       </NavigationMenu>
-      <div className="flex items-center">
+      <div className="flex items-center gap-2">
+        <ModeToggle />
         {isAuthenticated ? (
           <>
             <Button
@@ -90,7 +90,7 @@ export default function Header() {
             </Button>
             <Button
               variant={"default"}
-              className="ms-2 rounded-3xl"
+              className="rounded-3xl"
               onClick={logoutUser}
             >
               Déconnecter
@@ -104,13 +104,11 @@ export default function Header() {
             >
               <Link to={"/connexion"}>Se connecter</Link>
             </Button>
-            <Button variant={"default"} className="ms-2 rounded-3xl">
+            <Button variant={"default"} className="rounded-3xl">
               <Link to={"/inscription"}>Devenir membre</Link>
             </Button>
           </>
         )}
-        {/* <Button variant={"outline"} className="bg-transparent text-black border-black hover:bg-transparent rounded-3xl text-primary font-semibold hover:text-primary/90 transition-all duration-250">S'incrire</Button> */}
-        {/* <Button variant={"outline"} className="bg-transparent text-primary border-primary rounded-3xl font-semibold transition-all duration-250 hover:bg-primary/10">Se connecter</Button> */}
       </div>
     </header>
   );
