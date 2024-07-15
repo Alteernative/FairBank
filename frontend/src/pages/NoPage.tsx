e1a1b906a8d73016ecbb88f856397bd53fe3cb7d
Issam Khalladi <fc891922@ens.uqam.ca>
2024-07-15 19:42:44 -0400

    Fix image path

--- frontend/src/pages/NoPage.tsx
+++ frontend/src/pages/NoPage.tsx
@@ -4,8 +4,8 @@ import { Link } from "react-router-dom";
 export default function NoPage() {
   return (
     <div className="flex h-screen flex-col items-center justify-center">
-      <img src="/404_image.svg" alt="404 image" className="w-4/6"></img>
-      <Button asChild variant={"default"} className="rounded-3xl">
+      <img src="/images/404.svg" alt="404 image" className="w-4/6"></img>
+      <Button asChild variant={"ghost"} className="rounded-3xl">
         <Link to="/">Retourner à la page d'accueil</Link>
       </Button>
     </div>
