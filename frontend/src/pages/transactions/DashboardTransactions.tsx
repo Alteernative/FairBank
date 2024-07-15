4a61e8ec193ff13f4dddc8d5019a197d37c2e27a
Issam Khalladi <fc891922@ens.uqam.ca>
2024-07-15 19:46:25 -0400

    Update h1, format code

--- frontend/src/pages/transactions/DashboardTransactions.tsx
+++ frontend/src/pages/transactions/DashboardTransactions.tsx
@@ -5,33 +5,35 @@ import { DataTable } from "./data-table";
 export default function DashboardTransactions() {
   const { user } = useUserContext();
 
-  const sentTransactions: Payment[] = user.sent_transactions.map((transaction) => ({
-    id: transaction.id.toString(),
-    amount: parseFloat(transaction.amount),
-    status: "Envoyé",
-    email: transaction.receiver,
-    date: transaction.date,
-    type: "sent",
-  }));
+  const sentTransactions: Payment[] = user.sent_transactions.map(
+    (transaction) => ({
+      id: transaction.id.toString(),
+      amount: parseFloat(transaction.amount),
+      status: "Envoyé",
+      email: transaction.receiver,
+      date: transaction.date,
+      type: "sent",
+    })
+  );
 
-  const receivedTransactions: Payment[] = user.received_transactions.map((transaction) => ({
-    id: transaction.id.toString(),
-    amount: parseFloat(transaction.amount),
-    status: "Reçu",
-    email: transaction.sender,
-    date: transaction.date,
-    type: "received",
-  }));
+  const receivedTransactions: Payment[] = user.received_transactions.map(
+    (transaction) => ({
+      id: transaction.id.toString(),
+      amount: parseFloat(transaction.amount),
+      status: "Reçu",
+      email: transaction.sender,
+      date: transaction.date,
+      type: "received",
+    })
+  );
 
   const data: Payment[] = [...sentTransactions, ...receivedTransactions];
 
   return (
     <main className="h-full w-7/12 rounded-lg px-10 shadow-lg">
-      <h1 className="mb-10 font-jomhuria text-6xl">Historique des Transactions</h1>
-      <h2 className="text-2xl font-bold tracking-tight">Salut {user.first_name}!</h2>
-      <p className="text-muted-foreground">
-        Voici l'historique de tes transactions récentes:
-      </p>
+      <h1 className="mb-10 font-jomhuria text-6xl">
+        Historique des transactions
+      </h1>
       <DataTable columns={columns} data={data} />
     </main>
   );
