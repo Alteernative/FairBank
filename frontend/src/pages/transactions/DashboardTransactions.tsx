4694ee371142f678b56a204bbf1e9a7f44829e98
Jdaini Mohammed Ali <jdaini.mohammed_ali@courrier.uqam.ca>
2024-07-14 23:49:21 -0400

    Adding temp function to update the currency of the user

--- frontend/src/pages/transactions/DashboardTransactions.tsx
+++ frontend/src/pages/transactions/DashboardTransactions.tsx
@@ -1,34 +1,59 @@
 import { useUserContext } from "@/contexts/UserContext.tsx";
 import { Payment, columns } from "./columns";
 import { DataTable } from "./data-table";
+import AxiosInstance from "@/components/AxiosInstance.tsx";
 
 export default function DashboardTransactions() {
   const { user } = useUserContext();
 
-  const sentTransactions: Payment[] = user.sent_transactions.map((transaction) => ({
-    id: transaction.id.toString(),
-    amount: parseFloat(transaction.amount),
-    status: "Envoyé",
-    email: transaction.receiver,
-    date: transaction.date,
-    type: "sent",
-  }));
+  const updateCurrencyBalance = (currency, amount) => {
+    AxiosInstance.put(`currencies/update_balance/`, { currency, amount })
+      .then((response) => {
+        console.log("Balance updated successfully:", response.data);
+        // navigate("/dashboard");
+      })
+      .catch((error) => {
+        console.error("Error updating balance:", error);
+      });
+  };
 
-  const receivedTransactions: Payment[] = user.received_transactions.map((transaction) => ({
-    id: transaction.id.toString(),
-    amount: parseFloat(transaction.amount),
-    status: "Reçu",
-    email: transaction.sender,
-    date: transaction.date,
-    type: "received",
-  }));
+  const currency = "eur"; // Replace with the currency you want to update
+  const amount = 10.0; // Replace with the amount you want to add
+  console.log(currency, amount);
+  updateCurrencyBalance(currency, amount);
+
+  const sentTransactions: Payment[] = user.sent_transactions.map(
+    (transaction) => ({
+      id: transaction.id.toString(),
+      amount: parseFloat(transaction.amount),
+      status: "Envoyé",
+      email: transaction.receiver,
+      date: transaction.date,
+      type: "sent",
+    })
+  );
+
+  const receivedTransactions: Payment[] = user.received_transactions.map(
+    (transaction) => ({
+      id: transaction.id.toString(),
+      amount: parseFloat(transaction.amount),
+      status: "Reçu",
+      email: transaction.sender,
+      date: transaction.date,
+      type: "received",
+    })
+  );
 
   const data: Payment[] = [...sentTransactions, ...receivedTransactions];
 
   return (
     <main className="h-full w-7/12 rounded-lg px-10 shadow-lg">
-      <h1 className="mb-10 font-jomhuria text-6xl">Historique des Transactions</h1>
-      <h2 className="text-2xl font-bold tracking-tight">Salut {user.first_name}!</h2>
+      <h1 className="mb-10 font-jomhuria text-6xl">
+        Historique des Transactions
+      </h1>
+      <h2 className="text-2xl font-bold tracking-tight">
+        Salut {user.first_name}!
+      </h2>
       <p className="text-muted-foreground">
         Voici l'historique de tes transactions récentes:
       </p>
