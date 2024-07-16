bf26efbdbe3a93da51fe3afe563d5725287bb0d9
Issam Khalladi <fc891922@ens.uqam.ca>
2024-07-15 20:26:46 -0400

    Add appearance settings main

--- frontend/src/pages/dashboard/dashboard-settings/AppearanceSettings.tsx
+++ frontend/src/pages/dashboard/dashboard-settings/AppearanceSettings.tsx
@@ -1,3 +1,85 @@
+import { ModeToggle } from "@/components/ModeToggle";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
 export default function AppearanceSettings() {
-  return <main>Appearacne</main>;
+  return (
+    <main className="ml-14 mt-20 flex w-4/5 flex-col gap-4 md:ml-12 lg:ml-8">
+      <Card className="w-10/12 border-none shadow-none">
+        <CardHeader>
+          <CardTitle>Mode sombre</CardTitle>
+          <CardDescription>
+            Choisissez un mode entre clair, sombre et système.
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <ModeToggle />
+        </CardContent>
+      </Card>
+      <Card className="w-10/12 border-none shadow-none">
+        <CardHeader>
+          <CardTitle>Préférence de couleurs</CardTitle>
+          <CardDescription>
+            Choisissez un thème de couleurs parmis les palettes suivantes.
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          {/* TODO: Convert to a 3x3 grid */}
+          <section className="flex flex-wrap gap-2">
+            <Button
+              variant={"outline"}
+              className="flex w-32 items-center justify-start"
+            >
+              <div className="size-6 rounded-full border bg-green-500"></div>
+              <p className="ml-3">Vert</p>
+            </Button>
+            <Button
+              variant={"outline"}
+              className="flex w-32 items-center justify-start"
+            >
+              <div className="size-6 rounded-full border bg-blue-500"></div>
+              <p className="ml-3">Bleu</p>
+            </Button>
+            <Button
+              variant={"outline"}
+              className="flex w-32 items-center justify-start"
+            >
+              <div className="size-6 rounded-full border bg-red-500"></div>
+              <p className="ml-3">Rouge</p>
+            </Button>
+            <Button
+              variant={"outline"}
+              className="flex w-32 items-center justify-start"
+            >
+              <div className="size-6 rounded-full border bg-yellow-500"></div>
+              <p className="ml-3">Jaune</p>
+            </Button>
+            <Button
+              variant={"outline"}
+              className="flex w-32 items-center justify-start"
+            >
+              <div className="size-6 rounded-full border bg-purple-500"></div>
+              <p className="ml-3">Violet</p>
+            </Button>
+            <Button
+              variant={"outline"}
+              className="flex w-32 items-center justify-start"
+            >
+              <div className="size-6 rounded-full border bg-slate-500"></div>
+              <p className="ml-3">Gris</p>
+            </Button>
+          </section>
+        </CardContent>
+      </Card>
+      <Button type="submit" className="min-w-1/2 ml-6 mt-5 max-w-[10rem]">
+        Sauvegarder
+      </Button>
+    </main>
+  );
 }
