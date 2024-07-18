d9611fac08f533d96365879e18da08733ad5b0d1
Issam Khalladi <fc891922@ens.uqam.ca>
2024-07-18 12:57:14 -0400

    Replace name input with first name input and last name

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -17,16 +17,16 @@ export default function Footer() {
   };
 
   return (
-    <footer className="mb-3 mt-10 flex flex-col items-center gap-7">
+    <footer className="mb-3 mt-10 flex flex-col gap-7 md:items-center">
       {/* Top footer */}
-      <section className="flex w-full max-w-screen-xl flex-col gap-14 lg:flex-row lg:gap-0">
+      <section className="flex w-full max-w-screen-xl flex-col gap-14 md:flex-row md:gap-0">
         {/* Left */}
         <section className="flex flex-1 flex-col">
           <span className="cursor-default select-none">
             <h1 className="font-jomhuria text-6xl">FairBank</h1>
             <p>Banque différente, avenir&nbsp;différent.</p>
           </span>
-          <section className="mt-14 lg:mt-10">
+          <section className="mt-14 md:mt-10">
             <div className="flex items-center gap-3">
               <Phone size={"20"} />
               <Link to="tel:1-800-123-4567" className="hover:underline">
@@ -45,7 +45,7 @@ export default function Footer() {
           </section>
 
           {/* Socials */}
-          <section className="mt-10 flex gap-3 py-2 lg:mt-auto">
+          <section className="mt-10 flex gap-3 py-2 md:mt-auto">
             <Link to="https://facebook.com">
               <FaFacebookF className="cursor-pointer hover:opacity-70" />
             </Link>
@@ -66,9 +66,11 @@ export default function Footer() {
           </div> */}
         </section>
         {/* Middle */}
-        <section className="flex flex-1 flex-col text-center">
-          <h3 className="cursor-default text-lg font-semibold">Plan du site</h3>
-          <div className="flex flex-col gap-2 pt-5">
+        <section className="flex flex-1 flex-col text-start md:items-center md:text-center">
+          <h3 className="text-md mb-5 cursor-default font-semibold">
+            Plan du site
+          </h3>
+          <div className=" flex flex-col items-start gap-2 md:items-center">
             <Button asChild variant="link">
               <Link to={"/particuliers"} onClick={scrollToTop}>
                 Plans
@@ -97,13 +99,35 @@ export default function Footer() {
           </div>
         </section>
         {/* Right */}
-        <section className="flex w-full flex-1 flex-col gap-2 text-center lg:ml-10">
-          <h3 className="cursor-default text-lg font-semibold">
+        <section className="text-stard flex w-full flex-1 flex-col gap-2 md:ml-14 md:text-center">
+          <h3 className="text-md mb-5 cursor-default font-semibold">
             Contactez-nous
           </h3>
-          <Input id="name" placeholder="Nom" className="h-12 w-5/6" />
-          <Input type="email" placeholder="Email" className="h-12 w-5/6" />
-          <Textarea placeholder="Message" className="h-20 resize-none" />
+          <div className="flex gap-2">
+            <Input
+              id="first_name"
+              type="text"
+              placeholder="Prénom"
+              className="h-12 w-1/2"
+            />
+            <Input
+              id="last_name"
+              type="text"
+              placeholder="Nom"
+              className="h-12 w-1/2"
+            />
+          </div>
+          <Input
+            id="email"
+            type="text"
+            placeholder="Email"
+            className="h-12 w-full"
+          />
+          <Textarea
+            placeholder="Message"
+            autoComplete="off"
+            className="h-20 resize-none"
+          />
           <Button className="mt-2">Soumettre</Button>
         </section>
       </section>
