35bf81e4d23a6a17cca1170fc5a3c3aa9e638aa1
Issam Khalladi <fc891922@ens.uqam.ca>
2024-07-22 01:03:51 -0400

    Remove padding

--- frontend/src/pages/dashboard/dashboard-settings/Settings.tsx
+++ frontend/src/pages/dashboard/dashboard-settings/Settings.tsx
@@ -4,7 +4,7 @@ import { Toaster } from "sonner";
 
 export default function Settings({ children }: { children: React.ReactNode }) {
   return (
-    <section className="flex h-screen w-full py-7">
+    <section className="flex h-screen w-screen">
       <UserContextProvider>
         <SettingsSidebar />
         {children}
