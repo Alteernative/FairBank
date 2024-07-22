a7ebea06c3a6ab0240a453aa40c1a040ff263ec3
Issam Khalladi <fc891922@ens.uqam.ca>
2024-07-22 01:05:10 -0400

    Add responsive sidebar; Update to fixed position

--- frontend/src/components/SettingsSidebar.tsx
+++ frontend/src/components/SettingsSidebar.tsx
@@ -7,8 +7,18 @@ import {
   Bell,
   ArrowLeft,
   LockIcon,
+  Menu,
 } from "lucide-react";
 import { Separator } from "./ui/separator";
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "./ui/sheet";
 
 export default function SettingsSidebar() {
   const location = useLocation();
@@ -16,85 +26,188 @@ export default function SettingsSidebar() {
   const isActive = (path: string) => location.pathname === path;
 
   return (
-    <aside className="flex h-full w-1/5 flex-col items-center">
-      <Link to={"/"}>
-        <h1 className="font-jomhuria text-6xl">Paramètres</h1>
-        <Separator />
-      </Link>
+    // <aside className="sticky lg:w-2/12">
+    <aside className="fixed left-0 h-screen border-r py-5 lg:w-60">
+      <div className="hidden h-full w-full flex-col items-center lg:flex">
+        <Link to={"/"}>
+          <h1 className="font-jomhuria text-6xl">Paramètres</h1>
+        </Link>
 
-      <nav className="mb-5 mt-10 flex h-full flex-col justify-between">
-        {/* Top nav */}
-        <div className="flex flex-col gap-5">
-          <Button
-            variant="ghost"
-            className={`${isActive("/dashboard/settings") ? "bg-muted hover:bg-muted" : "hover:bg-transparent hover:underline"}`}
-          >
-            <Link
-              to={"/dashboard/settings"}
-              className="flex w-full items-center justify-start gap-2"
+        <nav className="mb-5 mt-10 flex h-full flex-col justify-between">
+          {/* Top nav */}
+          <div className="flex flex-col gap-5">
+            <Button
+              variant="ghost"
+              className={`${isActive("/dashboard/settings") ? "bg-muted hover:bg-muted" : "hover:bg-transparent hover:underline"}`}
             >
-              <UserRoundPen size={"20"} />
-              Profile
-            </Link>
-          </Button>
-          <Button
-            variant="ghost"
-            className={`${isActive("/dashboard/settings/account") ? "bg-muted hover:bg-muted" : "hover:bg-transparent hover:underline"}`}
-          >
-            <Link
-              to={"/dashboard/settings/account"}
-              className="flex w-full items-center justify-start gap-2"
+              <Link
+                to={"/dashboard/settings"}
+                className="flex w-full items-center justify-start gap-2"
+              >
+                <UserRoundPen size={"20"} />
+                Profile
+              </Link>
+            </Button>
+            <Button
+              variant="ghost"
+              className={`${isActive("/dashboard/settings/account") ? "bg-muted hover:bg-muted" : "hover:bg-transparent hover:underline"}`}
             >
-              <Wallet size={"20"} />
-              Compte
-            </Link>
-          </Button>
-          <Button
-            variant="ghost"
-            className={`${isActive("/dashboard/settings/themes") ? "bg-muted hover:bg-muted" : "hover:bg-transparent hover:underline"}`}
-          >
-            <Link
-              to={"/dashboard/settings/themes"}
-              className="flex w-full items-center justify-start gap-2"
+              <Link
+                to={"/dashboard/settings/account"}
+                className="flex w-full items-center justify-start gap-2"
+              >
+                <Wallet size={"20"} />
+                Compte
+              </Link>
+            </Button>
+            <Button
+              variant="ghost"
+              className={`${isActive("/dashboard/settings/themes") ? "bg-muted hover:bg-muted" : "hover:bg-transparent hover:underline"}`}
             >
-              <Palette size={"20"} />
-              Apparence
-            </Link>
-          </Button>
+              <Link
+                to={"/dashboard/settings/themes"}
+                className="flex w-full items-center justify-start gap-2"
+              >
+                <Palette size={"20"} />
+                Apparence
+              </Link>
+            </Button>
+            <Button
+              asChild
+              variant="ghost"
+              className={`flex w-full items-center justify-start gap-2 ${isActive("/dashboard/settings/notifications") ? "bg-muted hover:bg-muted" : "hover:bg-transparent hover:underline"}`}
+            >
+              <Link to={"/dashboard/settings/notifications"}>
+                <Bell size={"20"} />
+                Notifications
+              </Link>
+            </Button>
+            <Button
+              asChild
+              variant="ghost"
+              className={`flex w-full items-center justify-start gap-2 ${isActive("/dashboard/settings/display") ? "bg-muted hover:bg-muted" : "hover:bg-transparent hover:underline"}`}
+            >
+              <Link to={"/dashboard/settings/security"}>
+                <LockIcon size={"20"} />
+                Sécurité
+              </Link>
+            </Button>
+          </div>
+
+          {/* Bottom nav*/}
           <Button
             asChild
-            variant="ghost"
-            className={`flex w-full items-center justify-start gap-2 ${isActive("/dashboard/settings/notifications") ? "bg-muted hover:bg-muted" : "hover:bg-transparent hover:underline"}`}
+            variant={"ghost"}
+            className="items-center justify-start gap-2 font-medium"
           >
-            <Link to={"/dashboard/settings/notifications"}>
-              <Bell size={"20"} />
-              Notifications
+            <Link to={"/dashboard"}>
+              <ArrowLeft size={"20"} />
+              Retouner
             </Link>
           </Button>
-          <Button
-            asChild
-            variant="ghost"
-            className={`flex w-full items-center justify-start gap-2 ${isActive("/dashboard/settings/display") ? "bg-muted hover:bg-muted" : "hover:bg-transparent hover:underline"}`}
-          >
-            <Link to={"/dashboard/settings/security"}>
-              <LockIcon size={"20"} />
-              Sécurité
-            </Link>
-          </Button>
-        </div>
+        </nav>
+      </div>
 
-        {/* Bottom nav*/}
-        <Button
-          asChild
-          variant={"ghost"}
-          className="items-center justify-start gap-2 font-medium"
-        >
-          <Link to={"/dashboard"}>
-            <ArrowLeft size={"20"} />
-            Retouner
-          </Link>
-        </Button>
-      </nav>
+      {/* Responsive Sidebar */}
+      <div className="flex gap-4 lg:hidden">
+        <Sheet>
+          <SheetTrigger asChild>
+            <Button size={"icon"} variant={"ghost"} className="ml-2 mt-2">
+              <Menu size={30} />
+            </Button>
+          </SheetTrigger>
+          <SheetContent side={"left"} className="flex min-h-screen flex-col">
+            <SheetHeader className="hidden">
+              <SheetTitle />
+              <SheetDescription />
+            </SheetHeader>
+            <div className="flex h-full w-full flex-col items-start pl-10">
+              <Link to={"/"}>
+                <h1 className="font-jomhuria text-6xl">Paramètres</h1>
+                <Separator />
+              </Link>
+
+              <nav className="mb-5 mt-10 flex h-full flex-col justify-between">
+                {/* Top nav */}
+                <div className="flex flex-col gap-5">
+                  <SheetClose asChild>
+                    <Button
+                      asChild
+                      variant="ghost"
+                      className={`flex w-full items-center justify-start gap-2 ${isActive("/dashboard/settings") ? "bg-muted hover:bg-muted" : "hover:bg-transparent hover:underline"}`}
+                    >
+                      <Link to={"/dashboard/settings"}>
+                        <UserRoundPen size={"20"} />
+                        Profile
+                      </Link>
+                    </Button>
+                  </SheetClose>
+                  <SheetClose asChild>
+                    <Button
+                      asChild
+                      variant="ghost"
+                      className={`flex w-full items-center justify-start gap-2 ${isActive("/dashboard/settings/account") ? "bg-muted hover:bg-muted" : "hover:bg-transparent hover:underline"}`}
+                    >
+                      <Link to={"/dashboard/settings/account"}>
+                        <Wallet size={"20"} />
+                        Compte
+                      </Link>
+                    </Button>
+                  </SheetClose>
+                  <SheetClose asChild>
+                    <Button
+                      asChild
+                      variant="ghost"
+                      className={`flex w-full items-center justify-start gap-2 ${isActive("/dashboard/settings/themes") ? "bg-muted hover:bg-muted" : "hover:bg-transparent hover:underline"}`}
+                    >
+                      <Link to={"/dashboard/settings/themes"}>
+                        <Palette size={"20"} />
+                        Apparence
+                      </Link>
+                    </Button>
+                  </SheetClose>
+                  <SheetClose asChild>
+                    <Button
+                      asChild
+                      variant="ghost"
+                      className={`flex w-full items-center justify-start gap-2 ${isActive("/dashboard/settings/notifications") ? "bg-muted hover:bg-muted" : "hover:bg-transparent hover:underline"}`}
+                    >
+                      <Link to={"/dashboard/settings/notifications"}>
+                        <Bell size={"20"} />
+                        Notifications
+                      </Link>
+                    </Button>
+                  </SheetClose>
+                  <SheetClose asChild>
+                    <Button
+                      asChild
+                      variant="ghost"
+                      className={`flex w-full items-center justify-start gap-2 ${isActive("/dashboard/settings/display") ? "bg-muted hover:bg-muted" : "hover:bg-transparent hover:underline"}`}
+                    >
+                      <Link to={"/dashboard/settings/security"}>
+                        <LockIcon size={"20"} />
+                        Sécurité
+                      </Link>
+                    </Button>
+                  </SheetClose>
+                </div>
+
+                {/* Bottom nav*/}
+                <Button
+                  asChild
+                  variant={"ghost"}
+                  className="items-center justify-start gap-2 font-medium"
+                >
+                  <Link to={"/dashboard"}>
+                    <ArrowLeft size={"20"} />
+                    Retouner
+                  </Link>
+                </Button>
+              </nav>
+            </div>
+          </SheetContent>
+        </Sheet>
+      </div>
     </aside>
   );
 }
