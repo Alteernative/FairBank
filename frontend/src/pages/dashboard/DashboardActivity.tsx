34ad13d1fee7f8707378b061fd5c441e5d05f72c
Issam Khalladi <fc891922@ens.uqam.ca>
2024-07-26 00:12:31 -0400

    Update responsive design for pending table on mobile screens

--- frontend/src/pages/dashboard/DashboardActivity.tsx
+++ frontend/src/pages/dashboard/DashboardActivity.tsx
@@ -53,46 +53,48 @@ export default function DashboardActivity() {
                 <>
                   <div
                     key={index}
-                    className="flex items-center justify-between"
+                    className="flex flex-wrap items-center justify-between gap-4"
                   >
                     <div className="flex flex-col">
                       <p className="text-base">{transaction.receiver}</p>
                       <p className="text-sm text-muted-foreground">
                         {formatDate(transaction.date)}
                       </p>
-                      <div className="mt-5 flex items-center gap-3">
-                        <Button
-                          type="button"
-                          size={"icon"}
-                          variant={"outline"}
-                          onClick={() =>
-                            updateTransactionStatus(transaction, "accepted")
-                          }
-                          className="bg-green-500 text-white hover:bg-green-600 hover:text-white"
-                        >
-                          <Check />
-                        </Button>
-
-                        <Button
-                          type="button"
-                          size={"icon"}
-                          onClick={() =>
-                            updateTransactionStatus(transaction, "rejected")
-                          }
-                          className="bg-red-500 text-white hover:bg-red-600"
-                        >
-                          <X />
-                        </Button>
-                      </div>
                     </div>
                     <p className="select-none font-jomhuria text-4xl">
                       {formatCurrency(transaction.amount)}
                     </p>
-                    <p className="select-none font-medium text-blue-500">
+                    <div className="flex items-center gap-3">
+                      <Button
+                        type="button"
+                        size={"icon"}
+                        variant={"outline"}
+                        onClick={() =>
+                          updateTransactionStatus(transaction, "accepted")
+                        }
+                        className="bg-green-500 text-white hover:bg-green-600 hover:text-white"
+                      >
+                        <Check />
+                      </Button>
+
+                      <Button
+                        type="button"
+                        size={"icon"}
+                        onClick={() =>
+                          updateTransactionStatus(transaction, "rejected")
+                        }
+                        className="bg-red-500 text-white hover:bg-red-600"
+                      >
+                        <X />
+                      </Button>
+                    </div>
+
+                    {/* TODO: Is this necessary? */}
+                    {/* <p className="select-none font-medium text-blue-500">
                       {transaction.status === "pending"
                         ? "En attente"
                         : capitalize(transaction.status)}
-                    </p>
+                    </p> */}
                   </div>
                   <Separator />
                 </>
