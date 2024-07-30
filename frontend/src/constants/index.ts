d77a76aaabf613b61808494fbd0ace6dc193d72d
Issam Khalladi <fc891922@ens.uqam.ca>
2024-07-29 20:44:10 -0400

    Add more languages

--- frontend/src/constants/index.ts
+++ frontend/src/constants/index.ts
@@ -1,4 +1,13 @@
 export const LANGUAGES = [
   { language: "french", code: "fr" },
   { language: "english", code: "en" },
+  { language: "arabic", code: "ar" },
+  { language: "spanish", code: "es" },
+  { language: "russian", code: "ru" },
+  { language: "italian", code: "it" },
+  { language: "german", code: "de" },
+  { language: "japanese", code: "ja" },
+  { language: "chinese", code: "zh" },
+  { language: "portuguese", code: "pt" },
+  { language: "hindi", code: "hi" },
 ];
