353c3331149f4abee5613de888252d3bf84872cf
Issam Khalladi <fc891922@ens.uqam.ca>
2024-07-31 10:33:20 -0400

    Add language translation

--- frontend/src/schemas/SignUpSchema.ts
+++ frontend/src/schemas/SignUpSchema.ts
@@ -1,13 +1,16 @@
+import { useTranslation } from "react-i18next";
 import { z } from "zod";
+
+const { t } = useTranslation();
 
 const emailSchema = z.object({
   email: z
     .string()
     .min(1, {
-      message: "Adresse courriel requise.",
+      message: `${t("zod.signUp.email.min")}`,
     })
     .email({
-      message: "Adresse courriel invalide.",
+      message: `${t("zod.signUp.email.invalid")}`,
     }),
 });
 
@@ -16,21 +19,22 @@ const passwordSchema = z
     password: z
       .string()
       .min(8, {
-        message: "Mot de passe doit contenir au moins 8 caractères.",
+        // message: "Mot de passe doit contenir au moins 8 caractères.",
+        message: `${t("zod.signUp.password.min")}`,
       })
       .regex(/[A-Z]/, {
-        message: "Mot de passe doit contenir au moins une majuscule",
+        message: `${t("zod.signUp.password.upper")}`,
       })
       .regex(/[a-z]/, {
-        message: "Mot de passe doit contenir au moins une minuscule",
+        message: `${t("zod.signUp.password.lower")}`,
       })
       .regex(/[`!@#$%^&*()_\-+=\[\]{};':"\\|,.<>\/?~ ]/, {
-        message: "Mot de passe doit contenir au moins un caractère spécial.",
+        message: `${t("zod.signUp.password.special")}`,
       }),
     re_password: z.string(),
   })
   .refine((data) => data.password === data.re_password, {
-    message: "Les mots de passe ne sont pas identiques.",
+    message: `${t("zod.signUp.password.confirm")}`,
     path: ["re_password"],
   });
 
@@ -42,45 +46,49 @@ const nameSchema = z.object({
   first_name: z
     .string()
     .min(1, {
-      message: "Prénom requis.",
+      message: `${t("zod.signUp.name.firstName.min")}`,
     })
-    .regex(/^[A-Za-z]+$/, { message: "Prénom invalide." }),
+    .regex(/^[A-Za-z]+$/, {
+      message: `${t("zod.signUp.name.firstName.invalid")}`,
+    }),
   last_name: z
     .string()
     .min(1, {
-      message: "Nom requis.",
+      message: `${t("zod.signUp.name.lastName.min")}`,
     })
-    .regex(/^[A-Za-z]+$/, { message: "Nom invalide." }),
+    .regex(/^[A-Za-z]+$/, {
+      message: `${t("zod.signUp.name.lastName.invalid")}`,
+    }),
 });
 
 const birthdaySchema = z.object({
   birth_year: z.coerce
     .number({
-      invalid_type_error: "Année invalide.",
+      invalid_type_error: `${t("zod.signUp.birthday.year.invalid")}`,
     })
-    .min(1900, "Année invalide.")
-    .max(new Date().getFullYear(), "Année invalide")
+    .min(1900, `${t("zod.signUp.birthday.year.max")}`)
+    .max(new Date().getFullYear(), `${t("zod.signUp.birthday.year.invalid")}`)
     .refine(
       (year) => year <= new Date().getFullYear() - 18,
-      "Avoir au moins 18 ans."
+      `${t("zod.signUp.birthday.year.age")}`
     ),
 
   birth_month: z.string({
-    required_error: "Sélectionner le mois.",
+    required_error: `${t("zod.signUp.birthday.month.invalid")}`,
   }),
 
   birth_day: z.coerce
-    .number({ invalid_type_error: "Jour invalide." })
+    .number({ invalid_type_error: `${t("zod.signUp.birthday.day.min")}` })
     .min(1, {
-      message: "Jour invalide.",
+      message: `${t("zod.signUp.birthday.day.min")}`,
     })
     .max(31, {
-      message: "Jour invalide.",
+      message: `${t("zod.signUp.birthday.day.max")}`,
     }),
 });
 
 const planSchema = z.object({
-  plan: z.string().min(1, "Choisir un plan."),
+  plan: z.string().min(1, `${t("zod.signUp.plan.invalid")}`),
 });
 
 export const signUpSchema = (step) => {
