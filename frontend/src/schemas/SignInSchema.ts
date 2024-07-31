02cad8d52841f40097968ad6e5b8e3652638dad7
Issam Khalladi <fc891922@ens.uqam.ca>
2024-07-31 11:27:42 -0400

    Fix react hook error

--- frontend/src/schemas/SignInSchema.ts
+++ frontend/src/schemas/SignInSchema.ts
@@ -1,24 +1,26 @@
 import { useTranslation } from "react-i18next";
 import { z } from "zod";
 
-const { t } = useTranslation();
+export default function SignInSchema() {
+  const { t } = useTranslation();
 
-export const signInSchema = z.object({
-  email: z
-    .string()
-    .min(1, {
-      message: `${t("zod.signIn.email.min")}`,
-    })
-    .email({
-      message: `${t("zod.signIn.email.invalid")}`,
-    }),
+  z.object({
+    email: z
+      .string()
+      .min(1, {
+        message: `${t("zod.signIn.email.min")}`,
+      })
+      .email({
+        message: `${t("zod.signIn.email.invalid")}`,
+      }),
 
-  password: z
-    .string()
-    .min(1, {
-      message: `${t("zod.signIn.password.min")}`,
-    })
-    .min(8, {
-      message: `${t("zod.signIn.password.invalid")}`,
-    }),
-});
+    password: z
+      .string()
+      .min(1, {
+        message: `${t("zod.signIn.password.min")}`,
+      })
+      .min(8, {
+        message: `${t("zod.signIn.password.invalid")}`,
+      }),
+  });
+}
--- frontend/src/schemas/SignUpSchema.ts
+++ frontend/src/schemas/SignUpSchema.ts
@@ -1,97 +1,97 @@
 import { useTranslation } from "react-i18next";
 import { z } from "zod";
 
-const { t } = useTranslation();
+export default function SignUpSchema(step) {
+  const { t } = useTranslation();
 
-const emailSchema = z.object({
-  email: z
-    .string()
-    .min(1, {
-      message: `${t("zod.signUp.email.min")}`,
-    })
-    .email({
-      message: `${t("zod.signUp.email.invalid")}`,
-    }),
-});
-
-const passwordSchema = z
-  .object({
-    password: z
+  const emailSchema = z.object({
+    email: z
       .string()
-      .min(8, {
-        // message: "Mot de passe doit contenir au moins 8 caractères.",
-        message: `${t("zod.signUp.password.min")}`,
+      .min(1, {
+        message: `${t("zod.signUp.email.min")}`,
       })
-      .regex(/[A-Z]/, {
-        message: `${t("zod.signUp.password.upper")}`,
-      })
-      .regex(/[a-z]/, {
-        message: `${t("zod.signUp.password.lower")}`,
-      })
-      .regex(/[`!@#$%^&*()_\-+=\[\]{};':"\\|,.<>\/?~ ]/, {
-        message: `${t("zod.signUp.password.special")}`,
+      .email({
+        message: `${t("zod.signUp.email.invalid")}`,
       }),
-    re_password: z.string(),
-  })
-  .refine((data) => data.password === data.re_password, {
-    message: `${t("zod.signUp.password.confirm")}`,
-    path: ["re_password"],
   });
 
-const imageSchema = z.object({
-  image_url: z.any(),
-});
-
-const nameSchema = z.object({
-  first_name: z
-    .string()
-    .min(1, {
-      message: `${t("zod.signUp.name.firstName.min")}`,
+  const passwordSchema = z
+    .object({
+      password: z
+        .string()
+        .min(8, {
+          // message: "Mot de passe doit contenir au moins 8 caractères.",
+          message: `${t("zod.signUp.password.min")}`,
+        })
+        .regex(/[A-Z]/, {
+          message: `${t("zod.signUp.password.upper")}`,
+        })
+        .regex(/[a-z]/, {
+          message: `${t("zod.signUp.password.lower")}`,
+        })
+        .regex(/[`!@#$%^&*()_\-+=\[\]{};':"\\|,.<>\/?~ ]/, {
+          message: `${t("zod.signUp.password.special")}`,
+        }),
+      re_password: z.string(),
     })
-    .regex(/^[A-Za-z]+$/, {
-      message: `${t("zod.signUp.name.firstName.invalid")}`,
+    .refine((data) => data.password === data.re_password, {
+      message: `${t("zod.signUp.password.confirm")}`,
+      path: ["re_password"],
+    });
+
+  const imageSchema = z.object({
+    image_url: z.any(),
+  });
+
+  const nameSchema = z.object({
+    first_name: z
+      .string()
+      .min(1, {
+        message: `${t("zod.signUp.name.firstName.min")}`,
+      })
+      .regex(/^[A-Za-z]+$/, {
+        message: `${t("zod.signUp.name.firstName.invalid")}`,
+      }),
+    last_name: z
+      .string()
+      .min(1, {
+        message: `${t("zod.signUp.name.lastName.min")}`,
+      })
+      .regex(/^[A-Za-z]+$/, {
+        message: `${t("zod.signUp.name.lastName.invalid")}`,
+      }),
+  });
+
+  const birthdaySchema = z.object({
+    birth_year: z.coerce
+      .number({
+        invalid_type_error: `${t("zod.signUp.birthday.year.invalid")}`,
+      })
+      .min(1900, `${t("zod.signUp.birthday.year.max")}`)
+      .max(new Date().getFullYear(), `${t("zod.signUp.birthday.year.invalid")}`)
+      .refine(
+        (year) => year <= new Date().getFullYear() - 18,
+        `${t("zod.signUp.birthday.year.age")}`
+      ),
+
+    birth_month: z.string({
+      required_error: `${t("zod.signUp.birthday.month.invalid")}`,
     }),
-  last_name: z
-    .string()
-    .min(1, {
-      message: `${t("zod.signUp.name.lastName.min")}`,
-    })
-    .regex(/^[A-Za-z]+$/, {
-      message: `${t("zod.signUp.name.lastName.invalid")}`,
-    }),
-});
 
-const birthdaySchema = z.object({
-  birth_year: z.coerce
-    .number({
-      invalid_type_error: `${t("zod.signUp.birthday.year.invalid")}`,
-    })
-    .min(1900, `${t("zod.signUp.birthday.year.max")}`)
-    .max(new Date().getFullYear(), `${t("zod.signUp.birthday.year.invalid")}`)
-    .refine(
-      (year) => year <= new Date().getFullYear() - 18,
-      `${t("zod.signUp.birthday.year.age")}`
-    ),
+    birth_day: z.coerce
+      .number({ invalid_type_error: `${t("zod.signUp.birthday.day.min")}` })
+      .min(1, {
+        message: `${t("zod.signUp.birthday.day.min")}`,
+      })
+      .max(31, {
+        message: `${t("zod.signUp.birthday.day.max")}`,
+      }),
+  });
 
-  birth_month: z.string({
-    required_error: `${t("zod.signUp.birthday.month.invalid")}`,
-  }),
+  const planSchema = z.object({
+    plan: z.string().min(1, `${t("zod.signUp.plan.invalid")}`),
+  });
 
-  birth_day: z.coerce
-    .number({ invalid_type_error: `${t("zod.signUp.birthday.day.min")}` })
-    .min(1, {
-      message: `${t("zod.signUp.birthday.day.min")}`,
-    })
-    .max(31, {
-      message: `${t("zod.signUp.birthday.day.max")}`,
-    }),
-});
-
-const planSchema = z.object({
-  plan: z.string().min(1, `${t("zod.signUp.plan.invalid")}`),
-});
-
-export const signUpSchema = (step) => {
   switch (step) {
     case 1:
       return emailSchema;
@@ -106,4 +106,4 @@ export const signUpSchema = (step) => {
     case 6:
       return planSchema;
   }
-};
+}
