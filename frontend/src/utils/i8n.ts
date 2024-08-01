f31eda963571a2c11677fe2a718f450fad94c7c0
Issam Khalladi <fc891922@ens.uqam.ca>
2024-08-01 12:38:52 -0400

    Add arabic translation

--- frontend/src/utils/i8n.ts
+++ frontend/src/utils/i8n.ts
@@ -3,6 +3,7 @@ import { initReactI18next } from "react-i18next";
 import translationFR from "@/locales/fr/translation.json";
 import translationEN from "@/locales/en/translation.json";
 import translationES from "@/locales/es/translation.json";
+import translationAR from "@/locales/ar/translation.json";
 
 i18n.use(initReactI18next).init({
   lng: "fr",
@@ -13,6 +14,9 @@ i18n.use(initReactI18next).init({
     },
     en: {
       translation: translationEN,
+    },
+    ar: {
+      translation: translationAR,
     },
     es: {
       translation: translationES,
