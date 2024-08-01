328c9c4d1d8f60f909235bd53485047af3d82f64
Issam Khalladi <fc891922@ens.uqam.ca>
2024-08-01 19:24:06 -0400

    Move components from pages/ to components/

--- frontend/src/components/ProgressBar.tsx
+++ frontend/src/components/ProgressBar.tsx
@@ -1,8 +1,7 @@
 import { Progress } from "@/components/ui/progress";
-import * as React from "react";
 
 export function ProgressBar({ transactionsCount, upperLimit }) {
   const progress = (transactionsCount / upperLimit) * 100;
 
-  return <Progress value={progress} className="w-[100%]" />;
+  return <Progress value={progress} className="w-full" />;
 }
