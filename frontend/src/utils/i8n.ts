754b9573a5b8f9c6883fa2c3602ed11b00b4256f
Issam Khalladi <fc891922@ens.uqam.ca>
2024-08-01 12:44:44 -0400

    Add german translation

--- frontend/src/utils/i8n.ts
+++ frontend/src/utils/i8n.ts
@@ -4,6 +4,7 @@ import translationFR from "@/locales/fr/translation.json";
 import translationEN from "@/locales/en/translation.json";
 import translationES from "@/locales/es/translation.json";
 import translationAR from "@/locales/ar/translation.json";
+import translationDE from "@/locales/de/translation.json";
 
 i18n.use(initReactI18next).init({
   lng: "fr",
@@ -20,6 +21,9 @@ i18n.use(initReactI18next).init({
     },
     es: {
       translation: translationES,
+    },
+    de: {
+      translation: translationDE,
     },
   },
 });
