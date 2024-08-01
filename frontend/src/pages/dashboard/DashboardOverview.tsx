784e0297ff57a72639cef717d06b014ec760269e
Issam Khalladi <fc891922@ens.uqam.ca>
2024-08-01 19:21:39 -0400

    Add darker bg to cards

--- frontend/src/pages/dashboard/DashboardOverview.tsx
+++ frontend/src/pages/dashboard/DashboardOverview.tsx
@@ -7,7 +7,7 @@ import {
   CarouselContent,
   CarouselItem,
 } from "@/components/ui/carousel.tsx";
-import {Card, CardDescription, CardHeader} from "@/components/ui/card.tsx";
+import { Card, CardDescription, CardHeader } from "@/components/ui/card.tsx";
 import { ProgressBar } from "./ProgressBar";
 import { Trans, useTranslation } from "react-i18next";
 
@@ -42,8 +42,8 @@ export default function DashboardOverview() {
         {t("dashboard.overview.welcome")}
         {user.first_name}!
       </h1>
-      <div className="grid grid-cols-3 grid-rows-5 gap-4">
-        <div className="col-span-2 row-span-1 rounded-lg border p-4 shadow">
+      <div className="grid grid-cols-1 gap-4 lg:grid-cols-3 lg:grid-rows-5">
+        <div className="rounded-lg border bg-background p-4 shadow lg:col-span-2 lg:row-span-1 ">
           <h2 className="mb-3 font-bold">{t("dashboard.overview.balance")}</h2>
           <CountUp
             start={0}
@@ -55,31 +55,26 @@ export default function DashboardOverview() {
           />
         </div>
 
-        <div className="col-span-1 row-span-1 rounded-lg border p-4 shadow">
+        <div className="rounded-lg border bg-background p-4 shadow lg:col-span-1 lg:row-span-1">
           <h2 className="mb-3 font-bold">
             {t("dashboard.overview.transactions")}
           </h2>
           <ProgressBar
-              transactionsCount={user.sent_transactions.length}
-              upperLimit={UPPER_LIMIT}
-
+            transactionsCount={user.sent_transactions.length}
+            upperLimit={UPPER_LIMIT}
           />
         </div>
-        <div className="col-span-3 row-span-3 rounded-lg border p-4 shadow">
+        <div className="rounded-lg shadow lg:col-span-3 lg:row-span-3">
           <DashboardGraph />
         </div>
 
-        {/* <div className="col-span-1 row-span-3 rounded-lg border p-4 shadow">
-          <h2>Transactions rapide</h2>
-        </div> */}
-
-        <div className="col-span-3 row-span-1 rounded-lg border p-4 shadow">
-          <h2 className="mb-0 font-bold">
+        <div className="rounded-lg border bg-background p-4 shadow lg:col-span-3 lg:row-span-1">
+          <h2 className="mb-3 font-bold">
             {t("dashboard.overview.exchangeRate")}
           </h2>
 
           <Carousel className="w-full">
-            <CarouselContent className="-ml-1">
+            <CarouselContent>
               {Object.entries(user.currencies).map(([key, value], index) => {
                 const currencyKey = key.replace("balance_", "").toUpperCase();
 
@@ -88,7 +83,7 @@ export default function DashboardOverview() {
                     key={index}
                     className="md:basis-1/2 lg:basis-1/4"
                   >
-                    <Card className="flex h-20 flex-col items-center">
+                    <Card className="flex flex-col items-center">
                       <CardHeader className="flex cursor-default flex-col items-center">
                         <h2>
                           <CountUp
@@ -97,7 +92,7 @@ export default function DashboardOverview() {
                             duration={1}
                             prefix={currencySymbols[currencyKey]}
                             decimals={2}
-                            className="text-3xl font-extrabold"
+                            className="select-none text-3xl font-extrabold"
                           />
                         </h2>
                       </CardHeader>
@@ -108,6 +103,32 @@ export default function DashboardOverview() {
             </CarouselContent>
           </Carousel>
         </div>
+        {/* <Carousel className="w-full shadow lg:col-span-3 lg:row-span-1">
+          <CarouselContent className="-ml-1">
+            {Object.entries(user.currencies).map(([key, value], index) => {
+              const currencyKey = key.replace("balance_", "").toUpperCase();
+
+              return (
+                <CarouselItem key={index} className="md:basis-1/2 lg:basis-1/4">
+                  <Card className="flex h-20 flex-col items-center">
+                    <CardHeader className="flex cursor-default flex-col items-center">
+                      <h2>
+                        <CountUp
+                          start={0}
+                          end={parseFloat(value)}
+                          duration={1}
+                          prefix={currencySymbols[currencyKey]}
+                          decimals={2}
+                          className="text-3xl font-extrabold"
+                        />
+                      </h2>
+                    </CardHeader>
+                  </Card>
+                </CarouselItem>
+              );
+            })}
+          </CarouselContent>
+        </Carousel> */}
       </div>
     </main>
   );
