0484bd99cbd1217c91e0eef6d779255043ccb0c1
Issam Khalladi <fc891922@ens.uqam.ca>
2024-08-01 12:31:03 -0400

    Add spanish translation

--- frontend/src/utils/i8n.ts
+++ frontend/src/utils/i8n.ts
@@ -2,6 +2,7 @@ import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import translationFR from "@/locales/fr/translation.json";
 import translationEN from "@/locales/en/translation.json";
+import translationES from "@/locales/es/translation.json";
 
 i18n.use(initReactI18next).init({
   lng: "fr",
@@ -12,6 +13,9 @@ i18n.use(initReactI18next).init({
     },
     en: {
       translation: translationEN,
+    },
+    es: {
+      translation: translationES,
     },
   },
 });
