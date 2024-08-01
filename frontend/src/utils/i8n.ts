27e692f121827afc614d66b75949ca005728e800
Issam Khalladi <fc891922@ens.uqam.ca>
2024-08-01 13:18:18 -0400

    Add portuguese translation

--- frontend/src/utils/i8n.ts
+++ frontend/src/utils/i8n.ts
@@ -1,41 +1,45 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
-import translationFR from "@/locales/fr/translation.json";
-import translationEN from "@/locales/en/translation.json";
-import translationES from "@/locales/es/translation.json";
 import translationAR from "@/locales/ar/translation.json";
 import translationDE from "@/locales/de/translation.json";
+import translationEN from "@/locales/en/translation.json";
+import translationES from "@/locales/es/translation.json";
+import translationFR from "@/locales/fr/translation.json";
 import translationIT from "@/locales/it/translation.json";
-import translationRU from "@/locales/ru/translation.json";
 import translationJA from "@/locales/ja/translation.json";
+import translationPT from "@/locales/pt/translation.json";
+import translationRU from "@/locales/ru/translation.json";
 
 i18n.use(initReactI18next).init({
   lng: "fr",
   fallbackLng: "fr",
   resources: {
-    fr: {
-      translation: translationFR,
-    },
-    en: {
-      translation: translationEN,
-    },
     ar: {
       translation: translationAR,
-    },
-    es: {
-      translation: translationES,
     },
     de: {
       translation: translationDE,
     },
+    en: {
+      translation: translationEN,
+    },
+    es: {
+      translation: translationES,
+    },
+    fr: {
+      translation: translationFR,
+    },
     it: {
       translation: translationIT,
     },
-    ru: {
-      translation: translationRU,
-    },
     ja: {
       translation: translationJA,
+    },
+    pt: {
+      translation: translationPT,
+    },
+    ru: {
+      translation: translationRU,
     },
   },
 });
