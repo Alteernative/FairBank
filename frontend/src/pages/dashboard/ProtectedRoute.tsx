f50f165d7c5611a7b202f85ec855ef9351bed401
Jdaini Mohammed Ali <jdaini.mohammed_ali@courrier.uqam.ca>
2024-08-02 12:25:39 -0400

    Fixing the redirect when the user is not Authenticated

--- frontend/src/pages/dashboard/ProtectedRoute.tsx
+++ frontend/src/pages/dashboard/ProtectedRoute.tsx
@@ -31,7 +31,7 @@ const ProtectedRoute: React.FC<AdminProtectedRouteProps> = ({ element }) => {
   }
 
   // return isAuthenticated ? element : <Navigate to="/signin" />;
-  return isAuthenticated ? element : <Navigate to="/connexion" />;
+  return isAuthenticated ? element : <Navigate to="/signin" />;
 };
 
 export default ProtectedRoute;
