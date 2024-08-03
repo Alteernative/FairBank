23c47ad3c9c39ce696e5cde43bf2e857e457a14b
Issam Khalladi <fc891922@ens.uqam.ca>
2024-08-02 21:22:22 -0400

    Fix width on large screens

--- frontend/src/pages/NoPage.tsx
+++ frontend/src/pages/NoPage.tsx
@@ -7,8 +7,17 @@ export default function NoPage() {
 
   return (
     <div className="flex h-screen flex-col items-center justify-center">
-      <img src="/images/404.svg" alt="404 image" className="w-4/6"></img>
-      <Button asChild variant={"secondary"} className="rounded-3xl">
+      <img
+        src="/images/404.svg"
+        alt="404 image"
+        className="w-9/12 xl:w-[1000px]"
+      ></img>
+      <Button
+        asChild
+        variant={"secondary"}
+        className="mt-8
+       rounded-3xl"
+      >
         <Link to="/">{t("buttons.returnHome")}</Link>
       </Button>
     </div>
