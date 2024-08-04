8a5cf514a0210e4ea247b610a64d4851e8eb4617
Issam Khalladi <fc891922@ens.uqam.ca>
2024-08-04 00:18:54 -0400

    Update card height; Update send newletter button

--- frontend/src/pages/admin/dashboard/AdminNewsLetterSend.tsx
+++ frontend/src/pages/admin/dashboard/AdminNewsLetterSend.tsx
@@ -1,6 +1,7 @@
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import AxiosInstance from "@/components/AxiosInstance.tsx";
 import { Button } from "@/components/ui/button.tsx";
+import { Newspaper } from "lucide-react";
 
 const AdminNewsLetterSend = () => {
   const envoyerCourriels = async () => {
@@ -12,12 +13,17 @@ const AdminNewsLetterSend = () => {
   };
 
   return (
-    <Card className="mb-2 h-full lg:h-1/4">
+    <Card className="aspect-auto h-[125px] w-full xl:h-[150px]">
       <CardHeader className="text-center">
-        <CardTitle>Envoyer les Newsletter</CardTitle>
+        <CardTitle>Envoyer les Newsletters</CardTitle>
       </CardHeader>
       <CardContent className="flex flex-1 items-center justify-center ">
-        <Button onClick={envoyerCourriels}>Envoyer</Button>
+        <Button className="w-32" onClick={envoyerCourriels}>
+          <span className="flex items-center justify-center gap-4">
+            <Newspaper size={20} />
+            Envoyer
+          </span>
+        </Button>
       </CardContent>
     </Card>
   );
