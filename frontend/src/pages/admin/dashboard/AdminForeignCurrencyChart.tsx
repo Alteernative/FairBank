995344e96e29f1e4f1d2ac200206357f2991b347
Issam Khalladi <fc891922@ens.uqam.ca>
2024-08-05 18:48:06 -0400

    Update chart tooltip ui

--- frontend/src/pages/admin/dashboard/AdminForeignCurrencyChart.tsx
+++ frontend/src/pages/admin/dashboard/AdminForeignCurrencyChart.tsx
@@ -175,7 +175,7 @@ const AccountTypesChart = () => {
             />
             <ChartTooltip
               cursor={false}
-              content={<ChartTooltipContent hideLabel />}
+              content={<ChartTooltipContent indicator="line" />}
             />
             <Bar dataKey="Balance" layout="horizontal" radius={5} />
           </BarChart>
