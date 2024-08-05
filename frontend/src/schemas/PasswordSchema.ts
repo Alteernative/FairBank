8b06e73c6b96f88e6f1b03a2d29e6bccc1688dec
Issam Khalladi <fc891922@ens.uqam.ca>
2024-08-04 23:05:47 -0400

    Rename schema

--- frontend/src/schemas/PasswordSchema.ts
+++ frontend/src/schemas/PasswordSchema.ts
@@ -1,7 +1,7 @@
 import { useTranslation } from "react-i18next";
 import { z } from "zod";
 
-export default function ModifyPasswordSchema() {
+export default function PasswordSchema() {
   const { t } = useTranslation();
 
   return z
