476222471fe7fb2be05ba77be459364e484bf0d3
Dragomir <mirustance@yahoo.com>
2024-09-16 12:07:23 -0400

    fix settings for dynamic port binding

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -11,6 +11,6 @@ export default defineConfig({
   },
   server: {
     host: '0.0.0.0',
-    port: process.env.PORT || 3000,
+    port: process.env.PORT || 3000, // Use environment variable PORT
   }
 });
