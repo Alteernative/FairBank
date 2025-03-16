b692da571d7cd40abc555c695d78e9f505e09a0a
Alteernative <120146237+Alteernative@users.noreply.github.com>
2025-03-16 16:48:56 -0400

    Update AxiosInstance.tsx

--- frontend/src/components/AxiosInstance.tsx
+++ frontend/src/components/AxiosInstance.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const baseURl =
-  "http://fairbank-backend-38fb5c-6206e1-38-242-233-127.traefik.me/";
+  "backendfairbank-backend-uebaxj-1a7a55-82-112-240-23.traefik.me";
 
 const AxiosInstance = axios.create({
   baseURL: baseURl,
